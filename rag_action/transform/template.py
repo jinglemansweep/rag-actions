dc276ced73e28c685e9587ebb8b2cfbb5f2d779a
Louis King <jinglemansweep@gmail.com>
2025-06-01 14:01:36 +0100

    Updates

---
+++ rag_action/transform/template.py
@@ -0,0 +1,26 @@
+import logging
+from jinja2 import Template
+from ..config import get_env_var
+from ..utils import setup_logger, introduce, get_action_input, set_action_ouput
+
+setup_logger()
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+
+    template_text_input = get_env_var("TEMPLATE_TEXT")
+    input_json = get_action_input()
+
+    logger.info(
+        introduce(
+            "Transform Template",
+            {
+                "template_text": template_text_input,
+            },
+        )
+    )
+
+    rendered = Template(template_text_input).render(query="testquery", json=[])
+
+    set_action_ouput({"text": rendered})
